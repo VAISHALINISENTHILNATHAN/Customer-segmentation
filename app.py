d88ccfe7462777ee2df3a1ce9f1348ccc41ae003
Vaishalini <83913292+VAISHALINISENTHILNATHAN@users.noreply.github.com>
2025-11-23 11:29:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Customer Segmentation Dashboard",
+    page_icon="🛒",
+    layout="wide"
+)
+
+st.title("🛒 Customer Segmentation Platform")
+st.subheader("Advanced RFM + Machine Learning Dashboard")
+
+st.markdown("""
+### 👈 Use the sidebar to navigate between pages.
+This platform includes:
+
+- 📊 **RFM Analysis**
+- 🎯 **K-Means Clustering**
+- 📈 **Insights & Automated Reporting**
+""")
+
+
+# In[ ]:
+
+
+
+
